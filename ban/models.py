3b4c9397e9cd8c23a556a6b2803acf291ecd5341
BenZay <zynain@hotmail.com>
2017-03-08 22:22:10 +0100

    Compatible python 2.7

--- ban/models.py
+++ ban/models.py
@@ -1,6 +1,11 @@
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
+
 from django.db import models
 from django.conf import settings
 
+
+@python_2_unicode_compatible
 class UsersBan(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
                              verbose_name="User name",
@@ -15,4 +20,4 @@ class UsersBan(models.Model):
         ordering = ('user',)
 
     def __str__(self):
-        return '{}'.format(self.user)
+        return '{0}'.format(self.user)
